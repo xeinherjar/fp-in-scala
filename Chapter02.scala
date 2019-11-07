4e303c4105e9d2dee74d9557d37c4ca83d0cffd3
Alex Pate <alex.pate@bettercloud.com>
2019-11-07 08:12:17 -0500

    Chapter 02

--- Chapter02.scala
+++ Chapter02.scala
@@ -23,4 +23,7 @@ object Chapter02 {
 
     def uncurry[A, B, C](f: A => B => C): (A, B) => C =
         (a, b) => f(a)(b)
+    
+    def compose[A, B, C](f: B => C, g: A => B): A => C =
+        a => f(g(a))
 }
